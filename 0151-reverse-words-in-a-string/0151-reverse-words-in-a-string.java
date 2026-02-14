13b69f003978aece80d2dc50073588b2ae98a24c
riheasingh <riheasingh07234@gmail.com>
2026-02-14 20:38:47 +0530

    Time: 87 ms (5.34%), Space: 44.2 MB (80.11%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -18,7 +18,7 @@ class Solution {
         // }
         // return sb.toString();
 
-       String result=String.join(" ",str);
-       return result;
+     return String.join(" ",str);
+     
     }
 }
