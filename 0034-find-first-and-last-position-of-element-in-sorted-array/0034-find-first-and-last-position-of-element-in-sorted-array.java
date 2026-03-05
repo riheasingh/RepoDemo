f22a5b69a1a33faa0ada4fdd4609989796d49861
riheasingh <riheasingh07234@gmail.com>
2026-03-05 18:44:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,16 +1,65 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int first = -1;
-        int last = -1;
+    public int firstPosition(int[] nums, int target){
+        int low = 0;
+        int high = nums.length - 1;
+        int ans = -1;
 
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] == target){
-                if(first == -1){
-                    first = i;
-                }
-                last = i;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+
+            if(nums[mid] == target){
+                ans = mid;
+                high = mid - 1; // move left
+            }
+            else if(nums[mid] < target){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
             }
         }
+
+        return ans;
+    }
+
+    public int lastPosition(int[] nums, int target){
+        int low = 0;
+        int high = nums.length - 1;
+        int ans = -1;
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+
+            if(nums[mid] == target){
+                ans = mid;
+                low = mid + 1; // move right
+            }
+            else if(nums[mid] < target){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+    //     int first = -1;
+    //     int last = -1;
+    //     for(int i = 0; i < nums.length; i++){
+    //         if(nums[i] == target){
+    //             if(first == -1){
+    //                 first = i;
+    //             }
+    //             last = i;
+    //         }
+    //     }
+    // return new int[]{first, last};
+
+    int first = firstPosition(nums, target);
+    int last = lastPosition(nums, target);
 
         return new int[]{first, last};
     }
