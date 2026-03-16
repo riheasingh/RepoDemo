23314057e55393c38d4983697421e81dcd3c611e
riheasingh <riheasingh07234@gmail.com>
2026-03-17 00:30:22 +0530

    Time: 0 ms (100%), Space: 43.2 MB (61.55%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String findDifferentBinaryString(String[] nums) {
+         int n = nums.length;
+        StringBuilder ans = new StringBuilder();
+        
+        for(int i = 0; i < n; i++){
+            char c = nums[i].charAt(i);
+            
+            if(c == '0')
+                ans.append('1');
+            else
+                ans.append('0');
+        }
+        
+        return ans.toString();
+    }
+}
