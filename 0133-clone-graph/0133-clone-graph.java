9f4092d62fa9f7ca82ed096f87b1969ec124d436
riheasingh <riheasingh07234@gmail.com>
2026-03-17 11:48:13 +0530

    Time: 25 ms (79.83%), Space: 44.5 MB (35.26%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.java
@@ -0,0 +1,58 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+        // Map to store original -> clone mapping
+    HashMap<Node, Node> map = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+        if (node == null) return null;
+
+        map.clear();
+
+        // create clone of starting node
+        Node cloneNode = new Node(node.val);
+        map.put(node, cloneNode);
+
+        // DFS
+        dfs(node, cloneNode);
+
+        return cloneNode;
+    }
+
+    private void dfs(Node node, Node cloneNode) {
+        for (Node neighbor : node.neighbors) {
+
+            // if neighbor not cloned yet
+            if (!map.containsKey(neighbor)) {
+                Node newClone = new Node(neighbor.val);
+                map.put(neighbor, newClone);
+
+                cloneNode.neighbors.add(newClone);
+
+                dfs(neighbor, newClone);
+            } else {
+                // already cloned → just connect
+                cloneNode.neighbors.add(map.get(neighbor));
+            }
+        }
+    }
+
+}
