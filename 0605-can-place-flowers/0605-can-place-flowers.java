6a8840ca9aaaf83bfa78992a79dc64ed13396bae
riheasingh <riheasingh07234@gmail.com>
2026-03-20 16:43:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -1,25 +1,19 @@
 class Solution {
     public boolean canPlaceFlowers(int[] arr, int n) {
-        if(n==0) return true;
-        if(arr.length==1 && arr[0]==0 && n==1)return true; 
-         if(arr.length==1 && arr[0]==1 && n==1)return false; 
-         if(arr[0]==0 && arr[1]==0){
-            arr[0]=1;
-            n--;
-            if(n==0)return true;
-        }
-        for(int i=1;i<arr.length-1;i++){
-            if(arr[i]==0 && arr[i-1]==0 &&arr[i+1]==0){
-                arr[i]=1;
-                n--;
-                if(n==0)return true;
+        int len = arr.length;
+
+        for(int i = 0; i < len; i++){
+            if(arr[i] == 0){
+                int prev = (i == 0) ? 0 : arr[i-1];
+                int next = (i == len - 1) ? 0 : arr[i+1];
+
+                if(prev == 0 && next == 0){
+                    arr[i] = 1;  // plant flower
+                    n--;
+                    if(n <= 0) return true;
+                }
             }
         }
-        if(arr[arr.length-1]==0 && arr[arr.length-2]==0){
-            arr[arr.length-1]=1;
-               n--;
-              if(n==0)return true;
-        }
-        return false;
+        return n = 0;
     }
 }
