88c0c2389e442a8f33b5e658bc6832456ae1db98
riheasingh <riheasingh07234@gmail.com>
2026-03-25 16:56:06 +0530

    Time: 1 ms (33.15%), Space: 42.8 MB (46.69%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        Stack<Integer> open = new Stack<>();
+        Stack<Integer> star = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '(') {
+                open.push(i);
+            } 
+            else if (ch == '*') {
+                star.push(i);
+            } 
+            else { // ')'
+                if (!open.isEmpty()) {
+                    open.pop();
+                } 
+                else if (!star.isEmpty()) {
+                    star.pop();
+                } 
+                else {
+                    return false;
+                }
+            }
+        }
+
+        // Match remaining '(' with '*' (must be after it)
+        while (!open.isEmpty() && !star.isEmpty()) {
+            if (open.peek() < star.peek()) {
+                open.pop();
+                star.pop();
+            } else {
+                return false;
+            }
+        }
+
+        return open.isEmpty();
+        
+    }
+}
