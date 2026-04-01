ab44854ed509ba47ae6139e59c8af63f51fbee2c
riheasingh <riheasingh07234@gmail.com>
2026-04-01 11:10:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.java
+++ 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.java
@@ -1,13 +1,38 @@
 class Solution {
-    public int countPairs(int[] arr, int k) {
-        int count=0;
-        for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i]==arr[j]){
-                    if((i*j)%k==0)count++;
+    public int countPairs(int[] nums, int k) {
+    //     int count=0;
+    //     for(int i=0;i<arr.length;i++){
+    //         for(int j=i+1;j<arr.length;j++){
+    //             if(arr[i]==arr[j]){
+    //                 if((i*j)%k==0)count++;
+    //             }
+    //         }
+    //     }
+    //  return count;
+
+      Map<Integer, List<Integer>> map = new HashMap<>();
+        
+        // Step 1: store indices for each value
+        for (int i = 0; i < nums.length; i++) {
+            map.putIfAbsent(nums[i], new ArrayList<>());
+            map.get(nums[i]).add(i);
+        }
+        
+        int count = 0;
+        
+        // Step 2: check pairs inside each group
+        for (List<Integer> list : map.values()) {
+            int size = list.size();
+            
+            for (int i = 0; i < size; i++) {
+                for (int j = i + 1; j < size; j++) {
+                    if ((list.get(i) * list.get(j)) % k == 0) {
+                        count++;
+                    }
                 }
             }
         }
-     return count;
+        
+        return count;
     }
 }
