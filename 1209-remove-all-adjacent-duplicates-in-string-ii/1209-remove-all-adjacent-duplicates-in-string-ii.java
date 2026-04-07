16a70096884e3fb6c46dad267cd80936a08516f6
riheasingh <riheasingh07234@gmail.com>
2026-04-07 07:34:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,35 +1,59 @@
-import java.util.*;
+// class Solution {
+//     public String removeDuplicates(String s, int k) {
+        
+//         StringBuilder ans = new StringBuilder();
+//         Stack<Character> st = new Stack<>();
+//         Stack<Integer> count = new Stack<>();
+        
+//         for (int i = 0; i < s.length(); i++) {
+//             char ch = s.charAt(i);
+            
+//             if (!st.isEmpty() && st.peek() == ch) {
+//                 st.push(ch);
+//                 count.push(count.peek() + 1);
+//             } else {
+//                 st.push(ch);
+//                 count.push(1);
+//             }
+            
+//             if (count.peek() == k) {
+//                 for (int j = 0; j < k; j++) {
+//                     st.pop();
+//                     count.pop();
+//                 }
+//             }
+//         }
+        
+//         while (!st.isEmpty()) {
+//             ans.append(st.pop());
+//         }
+        
+//         return ans.reverse().toString();
+//     }
+// }
 
 class Solution {
     public String removeDuplicates(String s, int k) {
         
-        StringBuilder ans = new StringBuilder();
-        Stack<Character> st = new Stack<>();
-        Stack<Integer> count = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        int[] count = new int[s.length()];
         
         for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
+            sb.append(s.charAt(i));
             
-            if (!st.isEmpty() && st.peek() == ch) {
-                st.push(ch);
-                count.push(count.peek() + 1);
+            int last = sb.length() - 1;
+            
+            if (last > 0 && sb.charAt(last) == sb.charAt(last - 1)) {
+                count[last] = count[last - 1] + 1;
             } else {
-                st.push(ch);
-                count.push(1);
+                count[last] = 1;
             }
             
-            if (count.peek() == k) {
-                for (int j = 0; j < k; j++) {
-                    st.pop();
-                    count.pop();
-                }
+            if (count[last] == k) {
+                sb.delete(sb.length() - k, sb.length());
             }
         }
         
-        while (!st.isEmpty()) {
-            ans.append(st.pop());
-        }
-        
-        return ans.reverse().toString();
+        return sb.toString();
     }
 }
