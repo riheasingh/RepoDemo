19896d24b0f36a02a60e668082dcbd3917cf73f8
riheasingh <riheasingh07234@gmail.com>
2026-04-09 23:50:08 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -1,11 +1,40 @@
 class Solution {
     public String removeDuplicateLetters(String s) {
-              int n = s.length();
-        StringBuilder result = new StringBuilder();
+        //       int n = s.length();
+        // StringBuilder result = new StringBuilder();
+        
+        // boolean[] taken = new boolean[26];
+        // int[] lastIndex = new int[26];
+        // Arrays.fill(lastIndex, -1);
+        
+        // for (int i = 0; i < n; i++) {
+        //     char ch = s.charAt(i);
+        //     lastIndex[ch - 'a'] = i;
+        // }
+        
+        // for (int i = 0; i < n; i++) {
+        //     char ch = s.charAt(i);
+        //     int idx = ch - 'a';
+            
+        //     if (taken[idx]) continue;
+            
+        //     while (result.length() > 0 && ch < result.charAt(result.length() - 1) && lastIndex[result.charAt(result.length() - 1) - 'a'] > i) {
+        //         taken[result.charAt(result.length() - 1) - 'a'] = false;
+        //         result.deleteCharAt(result.length() - 1);
+        //     }
+            
+        //     result.append(ch);
+        //     taken[idx] = true;
+        // }
+        
+        // return result.toString();
+
+        //method2=stack
+        int n = s.length();
+        Stack<Character> st = new Stack<>();
         
         boolean[] taken = new boolean[26];
         int[] lastIndex = new int[26];
-        Arrays.fill(lastIndex, -1);
         
         for (int i = 0; i < n; i++) {
             char ch = s.charAt(i);
@@ -13,20 +42,23 @@ class Solution {
         }
         
         for (int i = 0; i < n; i++) {
-            char ch = s.charAt(i);
-            int idx = ch - 'a';
+            int idx = s.charAt(i) - 'a';
             
             if (taken[idx]) continue;
             
-            while (result.length() > 0 && ch < result.charAt(result.length() - 1) && lastIndex[result.charAt(result.length() - 1) - 'a'] > i) {
-                taken[result.charAt(result.length() - 1) - 'a'] = false;
-                result.deleteCharAt(result.length() - 1);
+            while (!st.isEmpty() && s.charAt(i) < st.peek() && lastIndex[st.peek() - 'a'] > i) {
+                taken[st.pop() - 'a'] = false;
             }
             
-            result.append(ch);
+            st.push(s.charAt(i));
             taken[idx] = true;
         }
         
-        return result.toString();
+        StringBuilder result = new StringBuilder();
+        while (!st.isEmpty()) {
+            result.append(st.pop());
+        }
+        
+        return result.reverse().toString();
     }
 }
