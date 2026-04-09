3d52c2a34bb6b122224e46c7e709d5aa1464d3c7
riheasingh <riheasingh07234@gmail.com>
2026-04-09 12:42:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,38 +1,77 @@
+// class Solution {
+//     public String minRemoveToMakeValid(String s) {
+//         Stack<Character> st=new Stack<>();
+//         int count=0;
+//         for(int i=0;i<s.length();i++){
+//             char ch=s.charAt(i);
+//             if(ch=='('){
+//                st.push(ch);
+//                count++;
+
+//             }
+//             else if(ch==')'){
+//                 if(count>0){
+//                     st.push(ch);
+//                     count--;
+//                 }
+                
+                
+//             }
+//             else st.push(ch);
+//         }
+//         StringBuilder sb=new StringBuilder();
+//         while(!st.isEmpty()){
+//             sb.append(st.pop());
+//         }
+//       sb=sb.reverse();
+//       for(int i=sb.length()-1;i>=0;i--){
+//         char ch=sb.charAt(i);
+//         if(count>0 && ch=='(') {
+//             sb.delete(i,i+1);
+//             count--;
+            
+//             }
+//       }
+
+//     return sb.toString();
+//     }
+// }
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        Stack<Character> st=new Stack<>();
-        int count=0;
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(ch=='('){
-               st.push(ch);
-               count++;
+        StringBuilder sb = new StringBuilder();
+        int open = 0;
 
-            }
-            else if(ch==')'){
-                if(count>0){
-                    st.push(ch);
-                    count--;
+        // Pass 1: remove extra ')'
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                open++;
+                sb.append(ch);
+            } 
+            else if (ch == ')') {
+                if (open > 0) {
+                    open--;
+                    sb.append(ch);
                 }
-                
-                
+            } 
+            else {
+                sb.append(ch);
             }
-            else st.push(ch);
         }
-        StringBuilder sb=new StringBuilder();
-        while(!st.isEmpty()){
-            sb.append(st.pop());
-        }
-      sb=sb.reverse();
-      for(int i=sb.length()-1;i>=0;i--){
-        char ch=sb.charAt(i);
-        if(count>0 && ch=='(') {
-            sb.delete(i,i+1);
-            count--;
-            
-            }
-      }
 
-    return sb.toString();
+        // Pass 2: remove extra '('
+        StringBuilder result = new StringBuilder();
+        int remove = open;
+
+        for (int i = sb.length() - 1; i >= 0; i--) {
+            char ch = sb.charAt(i);
+
+            if (ch == '(' && remove > 0) {
+                remove--;
+                continue;
+            }
+            result.append(ch);
+        }
+
+        return result.reverse().toString();
     }
 }
