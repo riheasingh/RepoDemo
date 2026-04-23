3b99d88cdefeaaf4d052ca656d8f9b46cd5b18fc
riheasingh <riheasingh07234@gmail.com>
2026-04-23 12:40:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,15 +1,19 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] arr, int target) {
-        Arrays.sort(arr);
+           Arrays.sort(arr);
+        HashSet<List<Integer>> set=new HashSet<>();
         List<List<Integer>> result=new ArrayList<>();
         List<Integer> current=new ArrayList<>();
-        backtrack(0,arr,target,current,result);
+        backtrack(0,arr,target,current,result,set);
         return result;
     }
-    public void backtrack(int i,int[] arr,int target,List<Integer> current,List<List<Integer>>result){
+    public void backtrack(int i,int[] arr,int target,List<Integer> current,List<List<Integer>>result, HashSet<List<Integer>> set){
         if(target==0){
-            
-            result.add(new ArrayList<>(current));
+           
+            if(!set.contains(current)){
+                result.add(new ArrayList<>(current));
+                set.add(new ArrayList<>(current));
+            }
             return;
         }
         if(target<0 || i==arr.length){
@@ -17,19 +21,15 @@ class Solution {
         }
         //Take
         current.add(arr[i]);
-        backtrack(i+1,arr,target-arr[i],current,result);
+        backtrack(i+1,arr,target-arr[i],current,result,set);
 
         //remove last one
         current.remove(current.size()-1);
+
         
-        // SKIP DUPLICATES
-        while (i + 1 < arr.length &&
-               arr[i] == arr[i + 1]) {
-            i++;
-        }
 
         //skip
-        backtrack(i+1,arr,target,current,result);
+        backtrack(i+1,arr,target,current,result,set);
 
     }
 }
