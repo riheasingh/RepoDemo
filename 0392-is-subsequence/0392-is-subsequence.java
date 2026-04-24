dda1fc1a13a048bffc2b8a427569d5a60534ffbe
riheasingh <riheasingh07234@gmail.com>
2026-04-24 12:37:54 +0530

    Time: 7 ms (2.39%), Space: 43.7 MB (6.43%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,20 +1,43 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
         
-        int i=0;
-        int j=0;
+    //     int i=0;
+    //     int j=0;
       
-        while( i<s.length() &&j<t.length()){
-            if(s.charAt(i)==t.charAt(j)){
-                i++;
-                j++;
-            }
-            else{
-                j++;
-            }
+    //     while( i<s.length() &&j<t.length()){
+    //         if(s.charAt(i)==t.charAt(j)){
+    //             i++;
+    //             j++;
+    //         }
+    //         else{
+    //             j++;
+    //         }
+    //     }
+    //     if(i!=s.length())return false;
+    //    return true;
+    int m=s.length(),n=t.length();
+    int[][]dp=new int[m+1][n+1];
+    for(int i=1;i<=m;i++){
+        for(int j=1;j<=n;j++){
+            if(s.charAt(i-1)==t.charAt(j-1)) dp[i][j]=1+dp[i-1][j-1];
+            else dp[i][j]=Math.max(dp[i][j-1],dp[i-1][j]);
         }
-        if(i!=s.length())return false;
-       return true; 
+    }
+    StringBuilder sb=new StringBuilder();
+    int i=m,j=n;
+    while(i>0 && j>0){
+        if(s.charAt(i-1)==t.charAt(j-1)){
+            sb.append(s.charAt(i-1));
+            i--;
+            j--;
+        }
+
+        else if(dp[i-1][j]>dp[i][j-1])i--;
+        else j--;
+    } 
+    sb.reverse();
+    if(sb.toString().equals(s))return true;
+    return false;
         
     }
 }
