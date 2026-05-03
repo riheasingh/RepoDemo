694339fd32c0eea7812fdf382cd5e23e06712627
riheasingh <riheasingh07234@gmail.com>
2026-05-03 12:29:07 +0530

    Time: 0 ms (100%), Space: 42.8 MB (58.69%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -15,20 +15,22 @@ class Solution {
        return true;
     }
     public boolean rotateString(String s, String goal) {
-        char[] arr=s.toCharArray();
-        int n=arr.length;
-        char[] target=goal.toCharArray();
-        int m=target.length;
-        if(m!=n) return false;
-        if (isEqual(arr, target)) return true;
-        for(int i=1;i<arr.length;i++){
-            reverse(arr,0,i-1);
-            reverse(arr,i,n-1);
-            reverse(arr,0,n-1);
-            boolean isEqual=isEqual(arr,target);
-            if(isEqual) return true;
-              arr = s.toCharArray();
-        }
-        return false;
+    //     char[] arr=s.toCharArray();
+    //     int n=arr.length;
+    //     char[] target=goal.toCharArray();
+    //     int m=target.length;
+    //     if(m!=n) return false;
+    //     if (isEqual(arr, target)) return true;
+    //     for(int i=0;i<arr.length;i++){
+    //         reverse(arr,0,i);
+    //         reverse(arr,i+1,n-1);
+    //         reverse(arr,0,n-1);
+    //         boolean isEqual=isEqual(arr,target);
+    //         if(isEqual) return true;
+    //         arr = s.toCharArray();
+    //     }
+    //     return false;
+        return s.length()==goal.length() && (s + s).contains(goal);
+
     }
 }
