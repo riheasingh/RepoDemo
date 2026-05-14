037d5a289f689811f5bac97bca24bd3440400987
riheasingh <riheasingh07234@gmail.com>
2026-05-14 07:58:08 +0530

    Time: 5 ms (48.38%), Space: 45.2 MB (20.19%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.java
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.java
@@ -1,14 +1,25 @@
 class Solution {
     public boolean isGood(int[] nums) {
-        Arrays.sort(nums);
-        int max=nums[nums.length-1];
+        // Arrays.sort(nums);
+        // int max=nums[nums.length-1];
         
-        int[] ans=new int[max+1];
+        // int[] ans=new int[max+1];
        
-        for(int i=0;i<max;i++){
-           ans[i]=i+1;
+        // for(int i=0;i<max;i++){
+        //    ans[i]=i+1;
+        // }
+        // ans[ans.length-1]=max;
+        // return Arrays.equals(nums,ans);
+        Arrays.sort(nums);
+
+        int n = nums.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] != i + 1) {
+                return false;
+            }
         }
-        ans[ans.length-1]=max;
-        return Arrays.equals(nums,ans);
+
+        return nums[n - 1] == n - 1;
     }
 }
