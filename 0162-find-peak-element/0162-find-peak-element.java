f0b6901a208f863189016f7afeb3df9dd629f60f
riheasingh <riheasingh07234@gmail.com>
2026-05-22 21:01:35 +0530

    Time: 0 ms (100%), Space: 44.5 MB (5.36%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -6,9 +6,22 @@ class Solution {
        
           if(nums[0]>nums[1]) return 0;
        
-        for(int i=1;i<=n-2;i++){
-            if(nums[i-1]<nums[i] && nums[i]>nums[i+1]) return i;
+        // for(int i=1;i<=n-2;i++){
+        //     if(nums[i-1]<nums[i] && nums[i]>nums[i+1]) return i;
            
+        // }
+        int i=1;
+        int j=n-2;
+
+        while(i<=j){
+            int mid=(i+j)/2;
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]) return mid;
+            else if(nums[mid]<nums[mid+1]){
+                 i=mid+1;
+            }
+            else{
+                 j=mid-1;
+            }
         }
        return n-1; 
     }
