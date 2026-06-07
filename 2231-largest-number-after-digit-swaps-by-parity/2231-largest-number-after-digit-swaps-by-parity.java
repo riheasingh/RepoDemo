e7cf33a67a5a12e92f596ef4fbc8163d9ae652ef
riheasingh <riheasingh07234@gmail.com>
2026-06-08 01:13:07 +0530

    Time: 3 ms (46.09%), Space: 42.4 MB (59.13%) - LeetHub

--- 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.java
+++ 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.java
@@ -1,28 +1,46 @@
 class Solution {
     public int largestInteger(int num) {
 
-        char[] arr = Integer.toString(num).toCharArray();
+        // char[] arr = Integer.toString(num).toCharArray();
 
-        for (int i = 0; i < arr.length; i++) {
+        // for (int i = 0; i < arr.length; i++) {
 
-            int maxPos = i;
+        //     int maxPos = i;
 
-            for (int j = i + 1; j < arr.length; j++) {
+        //     for (int j = i + 1; j < arr.length; j++) {
 
-                // same parity + larger digit
-                if (arr[j] > arr[maxPos] &&
-                   (arr[i] - arr[j]) % 2 == 0) {
+        //         // same parity + larger digit
+        //         if (arr[j] > arr[maxPos] &&
+        //            (arr[i] - arr[j]) % 2 == 0) {
 
-                    maxPos = j;
-                }
-            }
+        //             maxPos = j;
+        //         }
+        //     }
 
-            // swap
-            char temp = arr[i];
-            arr[i] = arr[maxPos];
-            arr[maxPos] = temp;
-        }
+        //     // swap
+        //     char temp = arr[i];
+        //     arr[i] = arr[maxPos];
+        //     arr[maxPos] = temp;
+        // }
 
-        return Integer.parseInt(new String(arr));
+        // return Integer.parseInt(new String(arr));
+
+
+        //Heap
+      PriorityQueue<Integer> even=new PriorityQueue<>(Collections.reverseOrder());
+      PriorityQueue<Integer> odd=new PriorityQueue<>(Collections.reverseOrder());
+         char[] arr = Integer.toString(num).toCharArray();
+         for(int i=0;i<arr.length;i++){
+            int val=arr[i]-'0';
+            if(val%2==0)even.add(val);
+            else odd.add(val);
+         }
+         StringBuilder sb=new StringBuilder();
+         for(int i=0;i<arr.length;i++){
+            int val=arr[i]-'0';
+            if(val%2==0)sb.append(even.poll());
+            else sb.append(odd.poll());
+         }
+         return Integer.parseInt(sb.toString());
     }
 }
