7f3278f7cd90640182593006c2be521d1b4c3715
riheasingh <riheasingh07234@gmail.com>
2026-06-09 20:52:16 +0530

    Time: 21 ms (18.86%), Space: 74.6 MB (9.21%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,34 +1,16 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] arr) {
-        // HashMap<Integer, Integer> map = new HashMap<>();
-        // int n = arr.length;
-        // List<Integer> list = new ArrayList<>();
-        // for (int i = 0; i < arr.length; i++) {
-        //     if (map.containsKey(arr[i])) {
-        //         map.put(arr[0], map.get(arr[i]) + 1);
-        //     } else
-        //         map.put(arr[i], 1);
-        // }
-        // for (int i = 1; i <= n; i++) {
-        //     if (map.containsKey(i))
-        //         continue;
-        //     else
-        //         list.add(i);
-        // }
-        // return list;
-
-
-        int n = arr.length;
-         List<Integer> list = new ArrayList<>();
-        boolean[] present=new boolean[n+1];
-        for(int i=0;i<n;i++){
-            if(arr[i]>=1 && arr[i]<=n){
-                present[arr[i]]=true;
-            }
+        List<Integer> list=new ArrayList<>();
+        HashSet<Integer> set=new HashSet<>();
+        for(int i=0;i<arr.length;i++){
+            set.add(arr[i]);
         }
-        for(int i=1;i<=n;i++){
-            if(!present[i])list.add(i);
-        }
+       for(int i=1;i<=arr.length;i++){
+        if(!set.contains(i))list.add(i);
+       }
         return list;
+
+
+        
    }
 }
