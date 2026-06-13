914747179df4315ef1e0b7735d2d6f4eacbfe72b
riheasingh <riheasingh07234@gmail.com>
2026-06-14 01:24:28 +0530

    Time: 0 ms (100%), Space: 43.8 MB (96.74%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -10,39 +10,28 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-    if(head == null || head.next == null) return head;
+       if(head==null || head.next==null)return head;
+        int count=0;
+     ListNode temp2=head;
+        while(temp2!=null){
+            temp2=temp2.next;
+            count++;
+        }
+        k=k%count;
+    if(k==0)return head;
+      ListNode temp=head;
+      for(int i=1;i<=count-k-1;i++){
+        temp=temp.next;
+      }
+      ListNode newHead=temp.next;
+      temp.next=null;
+      ListNode temp1=newHead;
+      while(temp1.next!=null){
+        temp1=temp1.next;
+      }
+      temp1.next=head;
+      return newHead;
 
-    ListNode temp = head;
-    int n = 0;
-
-    // count length
-    while(temp != null){
-        temp = temp.next;
-        n++;
-    }
-
-    k = k % n;
-    if(k == 0) return head;
-
-    ListNode slow = head;
-
-    // move to (n-k-1)th node
-    for(int i = 1; i <= n - k - 1; i++){
-        slow = slow.next;
-    }
-
-    ListNode newHead = slow.next;
-    slow.next = null;
-
-    // go to last node of new list
-    ListNode temp1 = newHead;
-    while(temp1.next != null){
-        temp1 = temp1.next;
-    }
-
-    // connect
-    temp1.next = head;
-
-    return newHead;
+   
 }
 }
