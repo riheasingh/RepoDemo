cc66e1e5e706e5a5df5066e2c583da1dd66ff078
riheasingh <riheasingh07234@gmail.com>
2026-08-09 20:36:41 +0530

    Time: 0 ms (100%), Space: 41.9 MB (83.26%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -2,7 +2,7 @@ class Solution {
     //Recursion +Memoisation
 
     public int path(int sr,int sc,int er,int ec,int[][]dp){
-        // if(sr==er || sc==ec)return 0;
+       // if(sr>=er || sc>=ec)return 0;
         if(sr==er-1 || sc==ec-1) return 1;
         if(dp[sr][sc]!=-1) return dp[sr][sc];
         int downways=path(sr+1,sc,er,ec,dp);
@@ -18,31 +18,5 @@ class Solution {
             }
         }
         return path(0,0,m,n,dp);
-
-        //int[][]dp=new int[m][n];
-       // for(int i=0;i<m;i++){
-        //     for(int j=0;j<n;j++){
-        //         if(i==0 || j==0) dp[i][j]=1;
-        //         else dp[i][j]=dp[i][j-1]+dp[i-1][j];
-        //     }
-        // }
-        // return dp[m-1][n-1];
-
-        // tabulation with space optimization
-        // int[][] dp=new int[2][n];
-        // for(int j=0;j<n;j++){
-        //     dp[0][j]=1;
-        //     dp[1][j]=1;
-        // }
-        // for(int i=1;i<m;i++){
-        //     for(int j=1;j<n;j++){
-        //         dp[1][j]=dp[1][j-1]+dp[0][j];
-        //     }
-        //     for(int j=1;j<n;j++){
-        //         dp[0][j]=dp[1][j];
-        //     }
-        // }
-        
-        // return dp[1][n-1];
     }
 }
