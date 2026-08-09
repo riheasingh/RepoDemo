827fc76a6e94d1f410992ef4f0a36616fb1c4170
riheasingh <riheasingh07234@gmail.com>
2026-08-09 17:16:55 +0530

    Time: 11 ms (66.95%), Space: 50.3 MB (20.44%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,13 +1,54 @@
 class Solution {
-    private int findDays(int[] arr,int cap){
-        int load=0;
-        int days=1;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]+load>cap){
+    //BRUTE FORCE
+
+
+    // private int findDays(int[] arr, int cap){
+    //     int load = 0;
+    //     int days = 1;
+
+    //     for(int i = 0; i < arr.length; i++){
+    //         if(arr[i] + load > cap){
+    //             days++;
+    //             load = arr[i];
+    //         } else {
+    //             load += arr[i];
+    //         }
+    //     }
+    //     return days;
+    // }
+
+    // public int shipWithinDays(int[] arr, int d) {
+    //     int max = Integer.MIN_VALUE;
+    //     int sum = 0;
+
+    //     for(int i = 0; i < arr.length; i++){
+    //         sum += arr[i];
+    //         max = Math.max(max, arr[i]);
+    //     }
+
+    //     // Brute force: try all capacities
+    //     for(int cap = max; cap <= sum; cap++){
+    //         int days = findDays(arr, cap);
+
+    //         if(days <= d){
+    //             return cap; // first valid minimum capacity
+    //         }
+    //     }
+
+    //     return -1; // never reached
+    // }
+
+    //METHOD2-BINARY SEARCH
+    private int findDays(int[] arr, int cap){
+        int load = 0;
+        int days = 1;
+
+        for(int i = 0; i < arr.length; i++){
+            load+=arr[i];
+            if(load>cap){
                 load=arr[i];
                 days++;
             }
-            else load+=arr[i];
         }
         return days;
     }
