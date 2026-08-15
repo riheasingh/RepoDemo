b6f1c625e66cc63c77f76c1ff79d53830fa518e5
riheasingh <riheasingh07234@gmail.com>
2026-08-15 22:25:10 +0530

    Time: 3 ms (99.95%), Space: 202.4 MB (88.1%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
@@ -10,31 +10,30 @@
  */
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
-        // ListNode slow=head;
-        // ListNode fast=head;
-        // if(head.next==null) head=null;
-        // else if(head.next.next==null) head.next=null;
-        //  else{
-        //     while(fast.next.next!=null && fast.next.next.next!=null){
-        //     slow=slow.next;
-        //     fast=fast.next.next;
-        //     }
-        //     slow.next=slow.next.next;
+        ListNode slow=head;
+        ListNode fast=head;
+        if(head.next==null) return null;
+        while(fast.next.next!=null && fast.next.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        
+    }
+    slow.next=slow.next.next;
+        
+        // if(head.next==null)return null;
+        // int count=0;
+        // ListNode temp=head;
+        // while(temp!=null){
+        //     temp=temp.next;
+        //     count++;
         // }
-        if(head.next==null)return null;
-        int count=0;
-        ListNode temp=head;
-        while(temp!=null){
-            temp=temp.next;
-            count++;
-        }
-        int n=count/2;
-        temp=head;
-        for(int i=1;i<n;i++){
-           temp=temp.next;
+        // int n=count/2;
+        // temp=head;
+        // for(int i=1;i<n;i++){
+        //    temp=temp.next;
 
-        }
-        temp.next=temp.next.next;
+        // }
+        // temp.next=temp.next.next;
         
         return head;
     
