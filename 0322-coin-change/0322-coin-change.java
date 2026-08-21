2e8b8880ca5c870efbe3e5cd461654491c25cfb5
riheasingh <riheasingh07234@gmail.com>
2026-08-21 17:10:04 +0530

    Time: 35 ms (13.1%), Space: 47.5 MB (10.14%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,25 +1,21 @@
 class Solution {
-    public long coinCount(int i,int[] coins,int amount,long[][]dp){
+     public int coinCount(int i,int[] coins,int amount,int[][] dp){
         if(i==coins.length){
-            if(amount==0) return 0;
+            if(amount==0)return 0;
             else return Integer.MAX_VALUE;
         }
-        if(dp[i][amount]!=-1) return dp[i][amount];
-        long skip=coinCount(i+1,coins,amount,dp);
+        if(dp[i][amount]!=-1)return dp[i][amount];
+        int skip=coinCount(i+1,coins,amount,dp);
         if(amount-coins[i]<0)return dp[i][amount]=skip;
-        long take=1+coinCount(i,coins,amount-coins[i],dp);
+        int take=coinCount(i,coins,amount-coins[i],dp);
+        if(take!=Integer.MAX_VALUE)take++;
         return dp[i][amount]=Math.min(skip,take);
     }
-    public int coinChange(int[] coins, int amount) {
-        long[][]dp=new long[coins.length][amount+1];
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<dp[0].length;j++){
-                dp[i][j]=-1;
-            }
-        }
-        int ans=(int)coinCount(0,coins,amount,dp);
-        if(ans==Integer.MAX_VALUE) return -1;
+    public int coinChange(int[] coins,int amount){
+        int[][] dp=new int[coins.length][amount+1];
+        for(int i=0;i<dp.length;i++)Arrays.fill(dp[i],-1);
+        int ans=coinCount(0,coins,amount,dp);
+        if(ans==Integer.MAX_VALUE)return -1;
         return ans;
-        
     }
 }
