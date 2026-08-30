5d488ce04e8a38bbf9d4cf4a45008bde53644fa4
riheasingh <riheasingh07234@gmail.com>
2026-08-31 03:13:26 +0530

    Time: 4 ms (11.09%), Space: 86.9 MB (8.36%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
@@ -1,22 +1,34 @@
 class Solution {
     public int minimumDeletions(int[] nums) {
-        int n = nums.length;
-
-        int minIndex = 0;
-        int maxIndex = 0;
-
-        for (int i = 1; i < n; i++) {
-            if (nums[i] < nums[minIndex]) minIndex = i;
-            if (nums[i] > nums[maxIndex]) maxIndex = i;
+        if(nums.length==1)return 1;
+        int n=nums.length;
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            min=Math.min(nums[i],min);
+            max=Math.max(nums[i],max);
         }
+        int posmax=-1;
+        int posmin=-1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==min)posmin=i;
+            if(nums[i]==max)posmax=i;
+        }
+        int minpos=-1;
+        int maxpos=-1;
+        if(posmax>posmin){
+            minpos=posmin;
+            maxpos=posmax;
+        }
+        else{
+            minpos=posmax;
+            maxpos=posmin;
+        }
+        
+        int del1=maxpos+1;
+        int del2=minpos+1+nums.length-maxpos;
+        int del3=n-minpos;
+        return Math.min(del1,Math.min(del2,del3));
 
-        int left = Math.min(minIndex, maxIndex);
-        int right = Math.max(minIndex, maxIndex);
-
-        int front = right + 1;
-        int back = n - left;
-        int both = (left + 1) + (n - right);
-
-        return Math.min(front, Math.min(back, both));
     }
 }
